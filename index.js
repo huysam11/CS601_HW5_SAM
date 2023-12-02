c31dd9116fcd4f1b6177853982f3fd17e81201b0
Huy Sam <huyprolove114@gmail.com>
2023-12-01 19:12:48 -0500

    update styles and script

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-window.onload = function() {
+window.onload = function () {
     // Display an alert when the page has completely loaded
     alert('Please click anywhere to display the table');
   };
-
-
-document.addEventListener('click', async () => { //Click event
+  
+  document.addEventListener('click', function () { //Click event
     const api = "https://raw.githubusercontent.com/huysam11/CS601_HW5_SAM/main/degrees.json";
-    try {
-        const response = await fetch(api); //Create a fetch request to return a promise
+    fetch(api)
+      .then(function (response) {
+        // Create a fetch request to return a promise
         if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.status}`);
+          throw new Error(`Network response was not ok: ${response.status}`);
         }
-        const data = await response.json(); // Parse the JSON data from the response
-        //Log the status code and received data to the console 
+        return response.json(); // Parse the JSON data from the response
+      })
+      .then(function (data) {
+        // Log the status code and received data to the console 
         console.log(`Status Code: ${data.status}`);
         console.log('Received Data:', data);
         var col = "";
-        data.forEach((itemData) => { //Create HTML string to display the data in a table
-            col += "<tr>";
-            col += "<td>" + itemData.school + "</td>";
-            col += "<td>" + itemData.major + "</td>";
-            col += "<td>" + itemData.type + "</td>";
-            col += "<td>" + itemData.year_conferred + "</td></tr>";
+        data.forEach(function (itemData) { // Create HTML string to display the data in a table
+          col += "<tr>";
+          col += "<td>" + itemData.school + "</td>";
+          col += "<td>" + itemData.major + "</td>";
+          col += "<td>" + itemData.type + "</td>";
+          col += "<td>" + itemData.year_conferred + "</td></tr>";
         });
-        document.getElementById('data').innerHTML = col; //Set inner HTML with id 'data'
-    } catch (error) {
-        console.error('Fetch Error:', error); //Handle errors
-    }
-});
+        document.getElementById('data').innerHTML = col; // Set inner HTML with id 'data'
+      })
+      .catch(function (error) {
+        console.error('Fetch Error:', error); // Handle errors
+      });
+  });
+  
